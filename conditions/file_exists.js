5ff8b804c8cfa7cf83aa45f52407ad1bb9a4d22c
Oleg <cheshire-mouse@users.noreply.github.com>
2012-02-02 16:06:57 +0000

    add condition script

---
+++ conditions/file_exists.js
@@ -0,0 +1,10 @@
+//check file existence
+//returns 	0 if file doesnt exist
+//		1 if file exists
+
+var fso=WScript.CreateObject("Scripting.FileSystemObject");
+
+var retCode=(WScript.Arguments.length>0 && fso.FileExists(WScript.Arguments(0)) ) ? 1 : 0;
+
+
+WScript.Quit(retCode);
